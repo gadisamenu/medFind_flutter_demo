e4eb248523e038fa4720fa6c864c11670daccc9e
habtamu <habtetsegaye19@gmail..com>
2022-06-06 04:16:47 +0300

    reservation some fix is done

--- lib/Infrastructure/Reservation/dataSource/local_data_provider.dart
+++ lib/Infrastructure/Reservation/dataSource/local_data_provider.dart
@@ -6,19 +6,7 @@ import 'package:medfind_flutter/Domain/Reservation/model.dart';
 import 'package:sqflite/sqflite.dart';
 import 'package:medfind_flutter/Infrastructure/_Shared/local_database.dart';
 
-// class SqlfliteDatasource implements ReservationProvider {
-//   final Database _db;
-//   const SqlfliteDatasource(@required Database db) : _db = db;
 
-//   @override
-//   Future<List<Reservation>?> getReservations() async{
-//     var listOfMaps = await _db.query()
-//   }
-//   Future<Reservation?> createReservation(){}
-//   Future<MedPack> deleteMedPack(double reservation_id, double medpack_id){}
-//   Future<MedPack> addMedPack(double reservation_id, double medpack_id){}
-//   Future<Reservation> DeleteReservation(double reservation_id){}
-// }
 class LocalReservationDataProvider extends SqliteDBProvider
     implements ReservationDataProvider {
   @override
@@ -27,12 +15,12 @@ class LocalReservationDataProvider extends SqliteDBProvider
     return queryResult.map((e) => Reservation.fromJson(e)).toList();
   }
 
-  // @override
-  // Future<Reservation> createReservation(
-  //     [int? medpack_id, int? pharmacy_id, Reservation? reservation]) async {
-  //   // insert("reservations", reservation!.toJson());
-  //   return reservation;
-  // }
+  @override
+  Future<Reservation> createReservation(
+      [int? medpack_id, int? pharmacy_id, Reservation? reservation]) async {
+    insert("reservations", reservation!.toJson());
+    return reservation;
+  }
  
   @override
   Future<void> deleteMedPack(int medpack_id, {int? reservation_id}) async {
@@ -43,12 +31,5 @@ class LocalReservationDataProvider extends SqliteDBProvider
   Future<void> deleteReservation(int reservation_id) async {
     delete("reservations", reservation_id);
   }
-  // @override
-  // Future<void> addMedPack(List<MedPack> medpack, int reservation_id) async {
-  //   updateFields("reservations", reservation_id, medPacks, medpack);
-  // }
-
-  // Future<void> addMedPack( MedPack medpacks,double reservation_id);
-  // Future<Reservation?> createReservation(List medpacks, int pharmacyId,){}
-
+  
 }
